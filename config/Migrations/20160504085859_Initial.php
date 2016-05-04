f6df4f518f17cc65f051d3199e1245caa402826b
robjuz <robjuz@gmail.com>
2016-05-04 10:39:42 +0200

    updated gitignore 	created migration file
    	modified:   .gitignore
    	new file:   config/Migrations/20160504083509_Initial.php

---
+++ config/Migrations/20160504085859_Initial.php
@@ -0,0 +1,157 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('kovicky_categories');
+        $table
+            ->addColumn('parent_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('rght', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('lft', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 45,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $table = $this->table('kovicky_mediafiles');
+        $table
+            ->addColumn('kovicky_solution_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('user_id', 'integer', [
+                'default' => 1,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('file_name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('file_url', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $table = $this->table('kovicky_problems');
+        $table
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('kovicky_category_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 60,
+                'null' => true,
+            ])
+            ->addColumn('thesis', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $table = $this->table('kovicky_solutions');
+        $table
+            ->addColumn('kovicky_problem_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('is_active', 'boolean', [
+                'default' => 0,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->droptable('kovicky_categories');
+        $this->droptable('kovicky_mediafiles');
+        $this->droptable('kovicky_problems');
+        $this->droptable('kovicky_solutions');
+    }
+}
