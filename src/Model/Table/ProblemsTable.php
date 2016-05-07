d8cd52c7394fe538c51ebe6adb7a6e43b5571506
robjuz <robjuz@gmail.com>
2016-05-07 09:48:07 +0200

    probme image first try 	new file:   config/Migrations/20160506173400_AddImageToProblems.php 	modified:   src/Model/Table/ProblemsTable.php

--- src/Model/Table/ProblemsTable.php
+++ src/Model/Table/ProblemsTable.php
@@ -33,6 +33,26 @@ class ProblemsTable extends Table
 
         $this->addBehavior('Timestamp');
         $this->addBehavior('Tree');
+        $this->addBehavior('Proffer.Proffer', [
+            'photo' => [    // The name of your upload field
+                'root' => WWW_ROOT . 'uploads', // Customise the root upload folder here, or omit to use the default
+                'dir' => 'photo_dir',   // The name of the field to store the folder
+                'thumbnailSizes' => [ // Declare your thumbnails
+                    'square' => [   // Define the prefix of your thumbnail
+                        'w' => 200, // Width
+                        'h' => 200, // Height
+                        'crop' => true,  // Crop will crop the image as well as resize it
+                        'jpeg_quality'  => 100,
+                        'png_compression_level' => 9
+                    ],
+                    'portrait' => [     // Define a second thumbnail
+                        'w' => 100,
+                        'h' => 300
+                    ],
+                ],
+                'thumbnailMethod' => 'imagick'  // Options are Imagick, Gd or Gmagick
+            ]
+        ]);
 
         $this->belongsTo('ParentProblems', [
             'className' => 'KoVicky.Problems',
